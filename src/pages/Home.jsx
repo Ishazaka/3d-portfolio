054ac558cafe6224d8e7b08a663fd4f747b6cf86
isha <iamatiger341@gmail.com>
2024-08-26 21:15:50 -0400

    added few files

---
+++ src/pages/Home.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import {Canvas} from '@react-three/fiber'
+
+const Home = () => {
+  return (
+
+  <section className='w-full h-screen relative'>
+{/* <div className='absolute top-28 left-0 right-0 z-10 flex item-center justify-center'>
+    POPUP
+</div> */}
+<Canvas className="w-full h-screen bg-transparent">
+   
+
+</Canvas>
+
+
+  </section>
+
+
+  )
+}
+
+export default Home
---
+++ src/pages/index.js
@@ -0,0 +1,7 @@
+import Home from "./Home";
+import Projects from './Projects'
+import Contact from './Contact'
+import About from './About'
+
+
+export {Home , Projects, Contact, About }
